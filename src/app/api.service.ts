2d59ffe04f4bb7cf5828bac300fd667a5156864f
lexlibra88 <98944627+lexlibra88@users.noreply.github.com>
2024-07-03 09:45:17 +0200

    VLM: F: Added endpoint allIngredients

---
+++ src/app/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private baseUrl = 'http://localhost:8081/api/v1/'; 
+
+  constructor(private http: HttpClient) {}
+
+  getData(endpoint: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${endpoint}`);
+  }
+
+  postData(endpoint: string, data: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/${endpoint}`, data);
+  }
+
+  getIngredients(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/allIngredients`);
+  }
+  // Puedes agregar más métodos según tus necesidades (put, delete, etc.)
+}
--- src/app/components/ingredient-list/ingredient-list.component.ts
+++ src/app/components/ingredient-list/ingredient-list.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ingredient-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ingredient-list.component.html',
   styleUrl: './ingredient-list.component.css'
 })
 export class IngredientListComponent {
+  recipes: any[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getIngredients().subscribe((data) => {
+      this.recipes = data;
+    });
+  }
 }
